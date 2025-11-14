de628eef2277ab5565ecff8bcc317cdf1449d8fc
koushalreddy11 <koushalabhay703@gmail.com>
2025-11-14 16:56:16 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('ms-v1').then(cache => cache.addAll(['/index.html','/manifest.json'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
